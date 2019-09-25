6a4025f5dcb789ea90cbc3337a1be5c869074b85
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-25 18:48:41 -0400

    Initial Commit of Spring Boot Project

---
+++ src/main/java/com/statsCalculatorGroup/statsCalculatorArtifact/StatsCalculatorArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.statsCalculatorGroup.statsCalculatorArtifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsCalculatorArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsCalculatorArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/statsCalculatorGroup/statsCalculatorArtifact/StatsCalculatorArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statsCalculatorGroup.statsCalculatorArtifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsCalculatorArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
